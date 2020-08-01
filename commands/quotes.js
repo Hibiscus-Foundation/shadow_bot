e028b4682ad897558b10128ecb11ca40dd1a547a
Purukitto <purukitto@gmail.com>
2020-08-01 12:11:58 +0530

    test quotes

--- commands/quotes.js
+++ commands/quotes.js
@@ -6,9 +6,7 @@ module.exports.run = async(bot, message, args) => {
     let ctip = ["Have a great day!", "Hope you are having a good day!", "Smile", "PSA, your smile is precious"]
     let tipn = Math.floor(Math.random() * 4);
     message.delete().catch(O_o => {});
-    let {
-        body
-    } = await agent.get('https://type.fit/api/quotes');
+    let body = await agent.get('https://type.fit/api/quotes');
     cicon = "https://i.imgur.com/HrQ300c.png";
     let cupt = new Discord.MessageEmbed()
         .setTitle("Quote time!")

9377ba3076d5e46234ca45aaa30f48908f4a7cf5
Purukitto <purukitto@gmail.com>
2020-08-01 12:48:35 +0530

    Quotes final text

--- commands/quotes.js
+++ commands/quotes.js
@@ -7,19 +7,17 @@ module.exports.run = async(bot, message, args) => {
     mchannel = message.channel;
     let ctip = ["Have a great day!", "Hope you are having a good day!", "Smile", "PSA, your smile is precious"]
     let tipn = Math.floor(Math.random() * 4);
+    let qq = body.length();
+    let qn = Math.floor(Math.random() * qq);
     // message.delete().catch(O_o => {});
-    // let {
-    //     body
-    // } = await agent.get('https://type.fit/api/quotes');
 
     cicon = "https://i.imgur.com/HrQ300c.png";
     let cupt = new Discord.MessageEmbed()
         .setTitle("Quote time!")
-        .setDescription(ctip[tipn])
         .setColor("#FF0000")
         .setThumbnail(cicon)
-        .addField(body[0].author, body[0].text)
-        .setFooter("Hope this helped :), woof!");
+        .addField(body[qn].author, body[qn].text)
+        .setFooter(ctip[tipn]);
 
     mchannel.send(cupt);
 }

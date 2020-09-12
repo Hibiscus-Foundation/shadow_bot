867eb90e6221c5d7f33b08be49f08e64b4b8d685
Purukitto <purukitto@gmail.com>
2020-09-12 22:25:22 +0530

    decode v1

--- commands/decode.js
+++ commands/decode.js
@@ -3,18 +3,18 @@ const agent = require('superagent');
 
 module.exports.run = async(bot, message, args) => {
     mchannel = message.channel;
-    morseArgs = args.join();
+    morseArgs = args.join().replace(',', '%20');
     console.log(morseArgs)
     let {
         body
     } = await agent.get('http://www.morsecode-api.de/decode?string=' + morseArgs);
 
     console.log(body.plaintext);
-    // let morseOut = new Discord.MessageEmbed()
-    //     .setTitle("Morse Decoder")
-    //     .setColor("#FF9900")
-    //     .addField("Translate:", body.plaintext);
-    // mchannel.send(morseOut);
+    let morseOut = new Discord.MessageEmbed()
+        .setTitle("Morse Decoder")
+        .setColor("#FF9900")
+        .addField("Translate:", body.plaintext);
+    mchannel.send(morseOut);
 }
 
 module.exports.help = {

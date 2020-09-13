01381c317d580498bd21a9559d4907e36c796f81
Purukitto <purukitto@gmail.com>
2020-09-13 12:20:33 +0530

    test vote

--- commands/vote.js
+++ commands/vote.js
@@ -6,31 +6,31 @@ module.exports.run = async(bot, message, args) => {
     }).then(res => {
         let lm = res.last();
         if (args[0] == 1) {
-            lm.react(':one:');
-            // if (error) console.error('One of the emojis failed to react.');
+            lm.react(' 🇦');
+            if (error) console.error('One of the emojis failed to react.');
         } else if (args[0] == 2) {
-            lm.react(':one:')
-                .then(() => lm.react(':two:'))
-                // .catch(() => console.error('One of the emojis failed to react.'));
+            lm.react(' 🇦')
+                .then(() => lm.react('🇧'))
+                .catch(() => console.error('One of the emojis failed to react.'));
         } else if (args[0] == 3) {
-            lm.react(':one:')
-                .then(() => lm.react(':two:'))
-                .then(() => lm.react(':three:'))
-                // .catch(() => console.error('One of the emojis failed to react.'));
+            lm.react(' 🇦')
+                .then(() => lm.react('🇧'))
+                .then(() => lm.react('🇨'))
+                .catch(() => console.error('One of the emojis failed to react.'));
         } else if (args[0] == 4) {
-            lm.react(':one:')
-                .then(() => lm.react(':two:'))
-                .then(() => lm.react(':three:'))
-                .then(() => lm.react(':four:'))
-                // .catch(() => console.error('One of the emojis failed to react.'));
+            lm.react(' 🇦')
+                .then(() => lm.react('🇧'))
+                .then(() => lm.react('🇨'))
+                .then(() => lm.react('🇩'))
+                .catch(() => console.error('One of the emojis failed to react.'));
 
         } else if (args[0] == 5 || !args[0]) {
-            lm.react(':one:')
-                .then(() => lm.react(':two:'))
-                .then(() => lm.react(':three:'))
-                .then(() => lm.react(':four:'))
-                .then(() => lm.react(':five:'))
-                // .catch(() => console.error('One of the emojis failed to react.'));
+            lm.react(' 🇦')
+                .then(() => lm.react('🇧'))
+                .then(() => lm.react('🇨'))
+                .then(() => lm.react('🇩'))
+                .then(() => lm.react('🇪'))
+                .catch(() => console.error('One of the emojis failed to react.'));
         }
     });
 }

c404cac14d7adebf8e1ac7494688f81ab58d0821
Purukitto <purukitto@gmail.com>
2020-09-13 12:11:30 +0530

    votes test

--- commands/vote.js
+++ commands/vote.js
@@ -4,31 +4,27 @@ module.exports.run = async(bot, message, args) => {
     message.channel.messages.fetch({
         limit: 2
     }).then(res => {
-        var no;
-        if (args[0])
-            no = args[0];
-        else no = 5;
         let lm = res.last();
-        if (no == 1) {
+        if (args[0] == 1) {
             lm.react(':one:');
             if (error) console.error('One of the emojis failed to react.');
-        } else if (no == 2) {
+        } else if (args[0] == 2) {
             lm.react(':one:')
                 .then(() => lm.react(':two:'))
                 .catch(() => console.error('One of the emojis failed to react.'));
-        } else if (no == 3) {
+        } else if (args[0] == 3) {
             lm.react(':one:')
                 .then(() => lm.react(':two:'))
                 .then(() => lm.react(':three:'))
                 .catch(() => console.error('One of the emojis failed to react.'));
-        } else if (no == 4) {
+        } else if (args[0] == 4) {
             lm.react(':one:')
                 .then(() => lm.react(':two:'))
                 .then(() => lm.react(':three:'))
                 .then(() => lm.react(':four:'))
                 .catch(() => console.error('One of the emojis failed to react.'));
 
-        } else if (no == 5) {
+        } else if (args[0] == 5 || !args[0]) {
             lm.react(':one:')
                 .then(() => lm.react(':two:'))
                 .then(() => lm.react(':three:'))

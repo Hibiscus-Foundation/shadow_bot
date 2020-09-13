b1fd375dc9d9a337de663cc82223d5f4668b3345
Purukitto <purukitto@gmail.com>
2020-09-13 12:34:55 +0530

    decode v2 test

--- commands/decode.js
+++ commands/decode.js
@@ -3,7 +3,7 @@ const agent = require('superagent');
 
 module.exports.run = async(bot, message, args) => {
     mchannel = message.channel;
-    morseArgs = args.join().replace(/,/g, '%20');
+    morseArgs = args.join().replace('`', '').replace(/,/g, '%20');
     let {
         body
     } = await agent.get('http://www.morsecode-api.de/decode?string=' + morseArgs);
@@ -12,6 +12,8 @@ module.exports.run = async(bot, message, args) => {
         .setColor("#FF9900")
         .setDescription(body.morsecode + " ➡ " + body.plaintext);
     mchannel.send(morseOut);
+
+    // if(body.plaintext == "F")
 }
 
 module.exports.help = {

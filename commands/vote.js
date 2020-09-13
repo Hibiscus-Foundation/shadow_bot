1e78ff3c522aff9de640b8a2e54d2f8dc461078f
Purukitto <purukitto@gmail.com>
2020-09-13 12:23:50 +0530

    test 2 vote

--- commands/vote.js
+++ commands/vote.js
@@ -7,22 +7,22 @@ module.exports.run = async(bot, message, args) => {
         let lm = res.last();
         if (args[0] == 1) {
             lm.react(' 🇦');
-            if (error) console.error('One of the emojis failed to react.');
+            // if (error) console.error('One of the emojis failed to react.');
         } else if (args[0] == 2) {
             lm.react(' 🇦')
                 .then(() => lm.react('🇧'))
-                .catch(() => console.error('One of the emojis failed to react.'));
+                // .catch(() => console.error('One of the emojis failed to react.'));
         } else if (args[0] == 3) {
             lm.react(' 🇦')
                 .then(() => lm.react('🇧'))
                 .then(() => lm.react('🇨'))
-                .catch(() => console.error('One of the emojis failed to react.'));
+                // .catch(() => console.error('One of the emojis failed to react.'));
         } else if (args[0] == 4) {
             lm.react(' 🇦')
                 .then(() => lm.react('🇧'))
                 .then(() => lm.react('🇨'))
                 .then(() => lm.react('🇩'))
-                .catch(() => console.error('One of the emojis failed to react.'));
+                // .catch(() => console.error('One of the emojis failed to react.'));
 
         } else if (args[0] == 5 || !args[0]) {
             lm.react(' 🇦')
@@ -30,7 +30,7 @@ module.exports.run = async(bot, message, args) => {
                 .then(() => lm.react('🇨'))
                 .then(() => lm.react('🇩'))
                 .then(() => lm.react('🇪'))
-                .catch(() => console.error('One of the emojis failed to react.'));
+                // .catch(() => console.error('One of the emojis failed to react.'));
         }
     });
 }

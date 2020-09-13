2563422121cd558231038aeb680f7894971b4cb8
Purukitto <purukitto@gmail.com>
2020-09-13 11:39:39 +0530

    react test

--- commands/react.js
+++ commands/react.js
@@ -1,14 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
-    let bicon = bot.user.displayAvatarURL();
-    let botembed = new Discord.MessageEmbed()
-        .setDescription("Bot Information")
-        .setColor("#00FFFF")
-        .setThumbnail(bicon)
-        .addField("Bot Name", bot.user.username)
-        .addField("Created on", bot.user.createdAt);
-    return message.channel.send(botembed);
+    message.channel.lastMessage.react('😄');
 }
 
 module.exports.help = {

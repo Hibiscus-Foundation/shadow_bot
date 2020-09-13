fe9afd59da66404a0138a20bc2097a57cd069854
Purukitto <purukitto@gmail.com>
2020-09-13 20:58:46 +0530

    vote v2 fix1

--- commands/vote.js
+++ commands/vote.js
@@ -1,11 +1,10 @@
 const Discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
-    message.delete().catch(O_o => {});
     message.channel.messages.fetch({
-        limit: 1
+        limit: 2
     }).then(res => {
-        let lm = res.first();
+        let lm = res.last();
         if (args[0] == 1) {
             lm.react('🇦');
         } else if (args[0] == 2) {
@@ -29,33 +28,7 @@ module.exports.run = async(bot, message, args) => {
                 .then(() => lm.react('🇪'))
         }
     });
-    // message.channel.messages.fetch({
-    //     limit: 2
-    // }).then(res => {
-    //     let lm = res.last();
-    //     if (args[0] == 1) {
-    //         lm.react('🇦');
-    //     } else if (args[0] == 2) {
-    //         lm.react('🇦')
-    //             .then(() => lm.react('🇧'))
-    //     } else if (args[0] == 3) {
-    //         lm.react('🇦')
-    //             .then(() => lm.react('🇧'))
-    //             .then(() => lm.react('🇨'))
-    //     } else if (args[0] == 4) {
-    //         lm.react('🇦')
-    //             .then(() => lm.react('🇧'))
-    //             .then(() => lm.react('🇨'))
-    //             .then(() => lm.react('🇩'))
-
-    //     } else if (args[0] == 5 || !args[0]) {
-    //         lm.react('🇦')
-    //             .then(() => lm.react('🇧'))
-    //             .then(() => lm.react('🇨'))
-    //             .then(() => lm.react('🇩'))
-    //             .then(() => lm.react('🇪'))
-    //     }
-    // });
+    message.delete().catch(O_o => {});
 }
 
 module.exports.help = {

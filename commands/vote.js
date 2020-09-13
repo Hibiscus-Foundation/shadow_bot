9f155c32547bb6e47e281f7188d324bf1e6902f6
Purukitto <purukitto@gmail.com>
2020-09-13 11:59:36 +0530

    vote test f

--- commands/vote.js
+++ commands/vote.js
@@ -6,9 +6,43 @@ module.exports.run = async(bot, message, args) => {
     }).then(res => {
         let no = args[0];
         let lm = res.last();
-        for (i = 0; i < no; i++) {
+        if (no == 1) {
             try {
-                lm.react(':one:');
+                await lm.react(':one:');
+            } catch (error) {
+                console.error('One of the emojis failed to react.');
+            }
+        } else if (no == 2) {
+            try {
+                await lm.react(':one:');
+                await lm.react(':two:');
+            } catch (error) {
+                console.error('One of the emojis failed to react.');
+            }
+        } else if (no == 3) {
+            try {
+                await lm.react(':one:');
+                await lm.react(':two:');
+                await lm.react(':three:');
+            } catch (error) {
+                console.error('One of the emojis failed to react.');
+            }
+        } else if (no == 4) {
+            try {
+                await lm.react(':one:');
+                await lm.react(':two:');
+                await lm.react(':three:');
+                await lm.react(':four:');
+            } catch (error) {
+                console.error('One of the emojis failed to react.');
+            }
+        } else if (no == 5) {
+            try {
+                await lm.react(':one:');
+                await lm.react(':two:');
+                await lm.react(':three:');
+                await lm.react(':four:');
+                await lm.react(':five:');
             } catch (error) {
                 console.error('One of the emojis failed to react.');
             }

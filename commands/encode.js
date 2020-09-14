bf8f24a406d8ed0ef95cb91a0e5d78744405d036
Purukitto <purukitto@gmail.com>
2020-09-15 00:14:30 +0530

    encode

---
+++ commands/encode.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+const agent = require('superagent');
+
+module.exports.run = async (bot, message, args) => {
+    mchannel = message.channel;
+    let {
+        body
+    } = await agent.get('http://www.morsecode-api.de/encode?string=' + morseArgs);
+    let morseOut = new Discord.MessageEmbed()
+        .setTitle("Morse Encoder")
+        .setColor("#FF9900")
+        .setDescription(body.plaintext + " ➡ " + body.morsecode);
+    mchannel.send(morseOut);
+
+}
+
+module.exports.help = {
+    name: "decode" //replace with call
+}

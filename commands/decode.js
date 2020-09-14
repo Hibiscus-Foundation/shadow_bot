d3d95ed1555e0866b3a9e700ed5d054211ce2f87
Purukitto <purukitto@gmail.com>
2020-09-14 22:18:36 +0530

    testing 2

--- commands/decode.js
+++ commands/decode.js
@@ -4,9 +4,9 @@ const agent = require('superagent');
 module.exports.run = async (bot, message, args) => {
     mchannel = message.channel;
     console.log(args)
-    // if (args[0] == " ") args[0] = "";
     morseArgs = args.join().replace(/`/g, '').replace(/,/g, '%20');
     if (morseArgs.slice(0, 2) == "%20") morseArgs.slice(3);
+    console.log(morseArgs.slice(0, 2))
     console.log(morseArgs) //TESTING ONLY
     let {
         body

8bc2a940ab8ed04018bd72d0a6d4b9b7a928996f
Purukitto <purukitto@gmail.com>
2020-09-14 23:33:18 +0530

    test dog

--- commands/dog.js
+++ commands/dog.js
@@ -6,16 +6,20 @@ module.exports.run = async (bot, message, args) => {
     let {
         body
     } = await agent.get('https://dog.ceo/api/breeds/image/random');
-    if (body.status == "success")
+    if (body.status == "success") {
         let cupt = new Discord.MessageEmbed()
             .setTitle("Doggo :dog:")
             .setColor("#FF9900")
             .setImage(body.message);
-    else let cupt = new Discord.MessageEmbed()
-        .setTitle("Doggo :dog:")
-        .setColor("#FF9900")
-        .setDescription("My doggo fren got lost! Can you use the command again! THANKS")
-    mchannel.send(cupt);
+        mchannel.send(cupt);
+    } else {
+        let cupt = new Discord.MessageEmbed()
+            .setTitle("Doggo :dog:")
+            .setColor("#FF9900")
+            .setDescription("My doggo fren got lost! Can you use the command again! THANKS");
+        mchannel.send(cupt);
+    }
+
 }
 
 module.exports.help = {

34b811e5338318532b236228d1ad0cfd2b1623b7
Purukitto <purukitto@gmail.com>
2020-09-15 15:24:54 +0530

    FIX DOGGO

--- commands/doggo.js
+++ commands/doggo.js
@@ -1,24 +1,17 @@
 const Discord = require("discord.js");
 const agent = require('superagent');
 
-module.exports.run = async (bot, message, args) => {
+module.exports.run = async(bot, message, args) => {
     mchannel = message.channel;
     let {
         body
-    } = await agent.get('https://dog.ceo/api/breeds/image/random');
-    if (body.status == "success") {
-        let cupt = new Discord.MessageEmbed()
-            .setTitle("Doggo :dog:")
-            .setColor("#FF9900")
-            .setImage(body.message);
-        mchannel.send(cupt);
-    } else {
-        let cupt = new Discord.MessageEmbed()
-            .setTitle("Doggo :dog:")
-            .setColor("#FF9900")
-            .setDescription("My doggo fren got lost! Can you use the command again! THANKS");
-        mchannel.send(cupt);
-    }
+    } = await agent.get('https://api.thedogapi.com/v1/images/search');
+
+    let cupt = new Discord.MessageEmbed()
+        .setTitle("Doggo :dog:")
+        .setColor("#FF9900")
+        .setImage(body[0].url);
+    mchannel.send(cupt);
 }
 
 module.exports.help = {

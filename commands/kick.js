789d9d51d1aa52fb2236df21490e2a64882489cb
L0SER <65214509+L0SER8228@users.noreply.github.com>
2020-10-02 17:58:36 +0530

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
     let kicon = message.mentions.users.first().displayAvatarURL();
 
     if (!message.guild.me.hasPermission("KICK_MEMBERS"))
-        return message.channel.send("I don't have enough permission to do that!")
+        return message.channel.send("I don't have enough permission to do that!");
 
     if (!message.member.hasPermission("KICK_MEMBERS"))
         return message.reply("Sorry, you don't have permissions to use this!");

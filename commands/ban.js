9859eacdfdc9d5f9b1b8ce2104ee088beb80ad8d
L0SER <65214509+L0SER8228@users.noreply.github.com>
2020-10-02 17:51:59 +0530

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -3,8 +3,8 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     let bicon = message.mentions.users.first().displayAvatarURL();
 
-   if (!message.guild.me.hasPermission("BAN_MEMBERS"))
-      return message.channel.send("I don't have enough permission to do that!")
+    if (!message.guild.me.hasPermission("BAN_MEMBERS"))
+        return message.channel.send("I don't have enough permission to do that!")
 
     if (!message.member.hasPermission("BAN_MEMBERS"))
         return message.reply("Sorry, you don't have permissions to use this!");

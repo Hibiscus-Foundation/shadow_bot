8dc721dc5fdfb875c9e7043556502ddb06c2177f
Purukitto <purukitto@gmail.com>
2020-10-03 10:12:44 +0530

    test

--- commands/id.js
+++ commands/id.js
@@ -2,13 +2,13 @@ const Discord = require("discord.js");
 const mergeImages = require('merge-images');
 
 module.exports.run = async(bot, message, args) => {
-    const user = message.mentions.users.first() || message.author;
-    console.log(user.roles.first())
+    const users = message.mentions.users.first() || message.author;
+    console.log(users.user.roles.first())
     let botembed = new Discord.MessageEmbed()
         .setDescription("Bot Information")
         .setColor("#00FFFF")
-        .setThumbnail(user.avatarURL)
-        .addField("User Name", user.username);
+        .setThumbnail(users.avatarURL)
+        .addField("User Name", users.username);
     return message.channel.send(botembed);
     mergeImages(['/body.png', '/eyes.png', '/mouth.png'])
         .then(b64 => document.querySelector('img').src = b64);

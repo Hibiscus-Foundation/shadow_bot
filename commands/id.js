1a45d20fc18e5b048150db099cedcce51f477727
Purukitto <purukitto@gmail.com>
2020-10-03 10:36:18 +0530

    id test

--- commands/id.js
+++ commands/id.js
@@ -2,18 +2,19 @@ const Discord = require("discord.js");
 const mergeImages = require('merge-images');
 
 module.exports.run = async(bot, message, args) => {
-    // const users = message.mentions.users.first() || message.author;
     let member = message.mentions.members.first();
     if (!member)
         member = message.member;
+
+    let uicon = mergeImages(['/assets/idbase.png', member.user.displayAvatarURL()]);
+
     let botembed = new Discord.MessageEmbed()
         .setDescription(member.roles.cache.first().name)
         .setColor(member.displayHexColor)
-        .setThumbnail(member.user.displayAvatarURL())
+        .setThumbnail(uicon)
+        // .setThumbnail(member.user.displayAvatarURL())
         .addField("User Name", member.displayName);
     return message.channel.send(botembed);
-    mergeImages(['/body.png', '/eyes.png', '/mouth.png'])
-        .then(b64 => document.querySelector('img').src = b64);
 }
 
 module.exports.help = {

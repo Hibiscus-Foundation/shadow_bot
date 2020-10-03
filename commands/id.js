76d8d25520935b122522809a3cd88faa8caaf7c4
Purukitto <purukitto@gmail.com>
2020-10-03 11:29:39 +0530

    test with canvas

--- commands/id.js
+++ commands/id.js
@@ -1,31 +1,31 @@
 const Discord = require("discord.js");
-const mergeImages = require('merge-images');
-const {
-    Canvas,
-    Image,
-    loadImage
-} = require('canvas');
-
+const Canvas = require('canvas');
 
 module.exports.run = async(bot, message, args) => {
     let member = message.mentions.members.first();
     if (!member)
         member = message.member;
-    // const uimg = await loadImage(member.user.displayAvatarURL());
-    console.log(member.user.displayAvatarURL())
-    const uimg = await loadImage(`${member.user.displayAvatarURL()}`);
+    const canvas = Canvas.createCanvas(324, 163);
+    const ctx = canvas.getContext('2d');
 
+    const background = await Canvas.loadImage('./idbase.png');
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
-    const uicon = await mergeImages(['./idbase.png', uimg], {
-        Canvas: Canvas,
-        Image: Image
-    });
+    ctx.beginPath();
+    ctx.arc(81.5, 81.5, 100, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.clip();
 
-    console.log(uicon);
+    const avatar = await Canvas.loadImage(member.user.displayAvatarURL({
+        format: 'jpg'
+    }));
+
+    ctx.drawImage(avatar, 13, 13, 150, 150);
+
     let botembed = new Discord.MessageEmbed()
         .setDescription(member.roles.cache.first().name)
         .setColor(member.displayHexColor)
-        // .setThumbnail(uicon)
+        .setThumbnail(canvas.toBuffer(), 'welcome-image.png')
         // .setThumbnail(member.user.displayAvatarURL())
         .addField("User Name", member.displayName);
     return message.channel.send(botembed);

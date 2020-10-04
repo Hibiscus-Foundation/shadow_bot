42a9271e0ba72061d743894066ed8c9082a4611d
L0SER <65214509+L0SER8228@users.noreply.github.com>
2020-10-04 18:18:11 +0530

    Created ban commad, updated kick commad (#4)

--- commands/ban.js
+++ commands/ban.js
@@ -1,46 +1,46 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    let kicon = message.mentions.users.first().displayAvatarURL();
+    let bicon = message.mentions.users.first().displayAvatarURL();
 
-    if (!message.guild.me.hasPermission("KICK_MEMBERS"))
+    if (!message.guild.me.hasPermission("BAN_MEMBERS"))
         return message.channel.send("I don't have enough permission to do that!");
 
-    if (!message.member.hasPermission("KICK_MEMBERS"))
+    if (!message.member.hasPermission("BAN_MEMBERS"))
         return message.reply("Sorry, you don't have permissions to use this!");
     let member = message.mentions.members.first();
     if (!member)
         return message.reply("Please mention a valid member of this server");
-    if (!member.kickable)
-        return message.reply("I cannot kick this user! Do they have a higher role?");
+    if (!member.banable)
+        return message.reply("I cannot ban this user! Do they have a higher role?");
     let reason = args.slice(1).join(' ');
     if (!reason)
-        return message.reply("Please indicate a reason for the kick!");
+        return message.reply("Please indicate a reason for the ban!");
 
-    let kembed = new Discord.MessageEmbed()
+    let bembed = new Discord.MessageEmbed()
         .setDescription("Moderator Log")
         .setColor("#FF0000")
-        .setThumbnail(kicon)
-        .addField("Kicked User", `${member}`)
+        .setThumbnail(bicon)
+        .addField("Banned User", `${member}`)
         .addField("Reason", reason)
         .addField("Channel", message.channel)
         .addField("Time", message.createdAt)
         .addField("Reported by", `${message.author} ID:${message.author.id}`);
 
-    await member.kick(reason)
-        .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
+    await member.ban(reason)
+        .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
     message.delete().catch(O_o => {});
-    message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`)
+    message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`)
         .then(msg => {
             msg.delete({
                 timeout: 30000
             });
         })
         .catch(console.log("ERROR"));
-    let kchannel = message.guild.channels.cache.find(ch => ch.name === 'reports');
-    kchannel.send(kembed);
+    let bchannel = message.guild.channels.cache.find(ch => ch.name === 'reports');
+    bchannel.send(bembed);
 }
 
 module.exports.help = {
-    name: "kick" //replace with call
+    name: "ban" //replace with call
 }
